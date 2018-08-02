bc2fc76244d4d518218229eed1585edf0f860a2e
qilanz2 <31163829+KyraZQL@users.noreply.github.com>
2018-08-01 21:39:32 -0500

    change color

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include_once 'conn.php';
 <body>
 
 <nav>
-    <div class="nav-wrapper">
+    <div class="nav-wrapper black">
         <a href="#" class="brand-logo">Road Trip Together</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="index.php">Home</a></li>
